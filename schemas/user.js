49f720ac54a44a8d93e95b7688cf68afdc5e218b
catprince <imcatimcool0909@126.com>
2016-06-17 18:02:04 +0800

    add signup

--- schemas/user.js
+++ schemas/user.js
@@ -30,14 +30,14 @@ UserSchema.pre('save', function(next){
     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
         if (err) return next(err);
 
-        bcrypt.hash(user.password, salt, function(err, hash){
+        bcrypt.hash(user.password, salt,'', function(err, hash){
             if (err) return next(err);
 
             user.password = hash;
             next();
         })
     })
-    next();
+    //next();
 });
 
 UserSchema.statics = {
