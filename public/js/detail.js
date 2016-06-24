ac02b26ae399137c2991c23ccd228b3ab074d569
catprince <imcatimcool0909@126.com>
2016-06-24 17:52:48 +0800

    add comment reply

---
+++ public/js/detail.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.comment').click(function(e){
+        var target = $(this);
+        var toId = target.data('tid');
+        var commentId = target.data('cid');
+       $('<input>').attr({
+           type: 'hidden',
+           id: 'toId',
+           name: 'comment[tid]',
+           value: toId
+       }).appendTo('#commentForm');
+
+        $('<input>').attr({
+            type: 'hidden',
+            id: 'commentId',
+            name: 'comment[cid]',
+            value: commentId
+        }).appendTo('#commentForm')
+    });
+});
