c9e73562392f38e08ed04a8a84a1d3550b9cf592
catprince <imcatimcool0909@126.com>
2016-06-30 10:38:08 +0800

    add pv

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -8,6 +8,11 @@ var path = require('path');
 //detail page
 exports.detail = function (req, res) {
     var id = req.params.id;
+    Movie.update({_id: id}, {$inc: {pv: 1}}, function(err){
+        if (err) {
+            console.log(err);
+        }
+    });
     Movie.findById(id, function (err, movie) {
         if (err) {
             console.log(err);
